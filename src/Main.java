4006f7c4bd6f987c27c6edc772036f6a01dfe870
ericatkins8 <eric.atkins@hilliardschools.org>
2022-09-15 13:42:23 -0400

    finished project

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        double price = 17.95;
+        double salesTax = 0.05;
+        double totalTax = price * salesTax;
+
+        System.out.println("Sales tax is " + totalTax + " additional dollars");
     }
 }
